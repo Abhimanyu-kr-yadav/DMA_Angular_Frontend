3684fa3cf6d3ff2f34476176d3299e4c6e1e32bd
Omkar5053 <omkar62006@gmail.com>
2022-07-14 00:05:58 +0530

    Home Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FetchserviceService } from './services/fetchservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'dma';
+  Sections:any[] = [];
+
+
+  constructor(private route: ActivatedRoute, private data: FetchserviceService){}
+ 
+  ngOnInit(): void {
+    this.data.GetSections().subscribe(section => this.Sections=section);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FetchserviceService } from './services/fetchservice.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    FetchserviceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
